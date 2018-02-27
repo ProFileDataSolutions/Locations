8b527976ca1466f728b917527026f062eb46ff02
Alfred Schmidt <alfred1963@me.com>
2018-02-28 10:00:35 +1100

    Initial Commit

---
+++ Locations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Locations
+//
+//  Created by Alfred Schmidt on 28/2/18.
+//  Copyright © 2018 Alfred Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
